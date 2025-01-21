2f8b8d532c6e7532a69d846b0c82620cd034e29d
灵汐管理员 <102273123+LynxiTech@users.noreply.github.com>
2025-01-21 11:52:23 +0800

    Update conf.py

--- source/conf.py
+++ source/conf.py
@@ -46,7 +46,7 @@ language = 'zh_CN'
 
 html_static_path = ['_static']
 html_permalinks_icon = '<span>#</span>'
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'sphinx_documatt_theme'
 
 html_css_files = [
     'table_word_wrap.css',
